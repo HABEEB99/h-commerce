119fd8f9fe92a6401e6f701e0d4312214ea5901a
Habeeb <habeebahmadu1@gmail.com>
2023-04-03 22:03:34 +0100

    feat: add responsive breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,21 @@ module.exports = {
         icon: "#D7CDCC",
         modal: "#59656F",
       },
+      animation: {
+        slide: "slide 25s linear infinite",
+      },
+      keyframes: {
+        slide: {
+          "0%,100%": { transform: "translateX(5%)" },
+          "50%": { transform: "translateX(-125%)" },
+        },
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
     },
   },
-  plugins: [],
+  plugins: [require("tailwind-scrollbar"), require("tailwind-scrollbar-hide")],
 };
