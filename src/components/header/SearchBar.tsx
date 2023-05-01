aa74ddea8748bafb55fc8a41169bea8ee85f42c6
Habeeb <habeebahmadu1@gmail.com>
2023-05-01 22:09:48 +0100

    fix: update search bar styles

--- src/components/header/SearchBar.tsx
+++ src/components/header/SearchBar.tsx
@@ -4,13 +4,15 @@ import { FaSearch } from "react-icons/fa";
 
 const SearchBar = () => {
   return (
-    <div className="hidden md:flex items-center justify-between h-10 w-96 rounded-full px-6 bg-slate-300 dark:bg-slate-100">
+    <div className="hidden md:flex items-center justify-between h-10 w-96 rounded-lg pl-4 bg-slate-300 dark:bg-slate-100 overflow-hidden border-btn">
       <input
         type="text"
         placeholder="Search for a product"
-        className="flex-1 outline-none h-full bg-slate-300 dark:bg-slate-100 rounded-l-full"
+        className="flex-1 outline-none h-full bg-slate-300 dark:bg-slate-100 rounded-l-full text-gray-700"
       />
-      <FaSearch className="icon" />
+      <div className="h-full w-16 flex items-center justify-center bg-btn rounded-r-lg">
+        <FaSearch className="icon" />
+      </div>
     </div>
   );
 };
