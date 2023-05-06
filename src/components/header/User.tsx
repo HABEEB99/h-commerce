c857aa68a342c90012d65dc7ba12cf4acd37c23e
Habeeb <habeebahmadu1@gmail.com>
2023-05-06 23:57:58 +0100

    feat: add user component

---
+++ src/components/header/User.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { FaUser } from "react-icons/fa";
+
+const User = () => {
+  return (
+    <>
+      <FaUser className="text-xl text-icon cursor-pointer font-bold" />
+    </>
+  );
+};
+
+export default User;
